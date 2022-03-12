19437a79c67a3cc2bafe2cfd554d8a137b631819
Peter Csajtai <peter.csajtai@outlook.com>
2022-03-12 17:54:27 +0100

    Bump Stashbox version

--- patch-version.ps1
+++
@@ -1,8 +0,0 @@
-param($projectPath, $version)
-
-Write-Host "Patching with version $version"
-
-$csprojPath = Join-Path $PSScriptRoot $projectPath
-[xml]$project = Get-Content -Path $csprojPath
-$project.Project.PropertyGroup.Version = $version
-$project.Save($csprojPath)
